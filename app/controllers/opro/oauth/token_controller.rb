6b7aa911fd26fdc73d819f26a825e7f5cccb533d
Adam Duston <aduston@gmail.com>
2016-07-19 13:55:53 -0400

    Skipping authenticate_user on call to token/authorize. To determine user, we find an email that matches the login parameter in the same org as the 'authenticated user'.

--- app/controllers/opro/oauth/token_controller.rb
+++ app/controllers/opro/oauth/token_controller.rb
@@ -2,17 +2,24 @@
 # codes and refresh_tokens for access_tokens
 
 class Opro::Oauth::TokenController < OproController
-  before_filter      :opro_authenticate_user!,    :except => [:create]
-  skip_before_filter :verify_authenticity_token,  :only   => [:create]
-
+  before_filter :opro_authenticate_user!, except: [:create]
+  skip_before_filter :verify_authenticity_token, only: [:create]
+  skip_before_filter :authenticate_user!, only: [:create]
 
   def create
     # Find the client application
-    application = Opro::Oauth::ClientApp.authenticate(params[:client_id], params[:client_secret])
-
-    if application.present? && (@auth_grant = auth_grant_for(application, params)).present?
-      @auth_grant.refresh!
-      render :create
+    application = Opro::Oauth::ClientApp.authenticate(
+      params[:client_id], params[:client_secret])
+    auth_grant = auth_grant_for(application, params)
+    if application.present? && auth_grant.present?
+      auth_grant.refresh!
+      json_obj = {
+        access_token: auth_grant.access_token,
+        token_type: 'bearer',
+        refresh_token: auth_grant.refresh_token,
+        expires_in: auth_grant.expires_in
+      }
+      render json: json_obj
     else
       render_error debug_msg(params, application)
     end
@@ -25,14 +32,6 @@ class Opro::Oauth::TokenController < OproController
       Opro::Oauth::AuthGrant.find_by_code_app(params[:code], application)
     elsif params[:refresh_token]
       Opro::Oauth::AuthGrant.find_by_refresh_app(params[:refresh_token], application)
-    elsif params[:password].present? || params[:grant_type] == "password"|| params[:grant_type] == "bearer"
-      return false unless Opro.password_exchange_enabled?
-      return false unless oauth_valid_password_auth?(params[:client_id], params[:client_secret])
-      user       = ::Opro.find_user_for_all_auths!(self, params)
-      return false unless user.present?
-      auth_grant = Opro::Oauth::AuthGrant.find_or_create_by_user_app(user, application)
-      auth_grant.update_permissions if auth_grant.present?
-      auth_grant
     end
   end
 
@@ -48,5 +47,4 @@ class Opro::Oauth::TokenController < OproController
   def render_error(msg)
     render :json => {:error => msg }, :status => :unauthorized
   end
-
 end
